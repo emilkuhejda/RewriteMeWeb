1d63ac02ca7941d113564d61e4da88f77d4f8716
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-03-01 22:28:28 +0100

    Create User and Authentication controller.

---
+++ WebApi/RewriteMe.WebApi/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RewriteMe.Domain.Interfaces.Services;
+using RewriteMe.WebApi.Extensions;
+using RewriteMe.WebApi.Models;
+
+namespace RewriteMe.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [Authorize]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IAuthenticationService _authenticationService;
+
+        public UserController(IUserService userService, IAuthenticationService authenticationService)
+        {
+            _userService = userService;
+            _authenticationService = authenticationService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("/api/users/register")]
+        public IActionResult Register([FromBody] RegisterUserModel registerUserModel)
+        {
+            var user = registerUserModel.ToUser();
+            if (_userService.UserAlreadyExists(user))
+                return Conflict();
+
+            _authenticationService.CalculatePasswordHash(user, registerUserModel.Password);
+            _userService.Add(user);
+
+            return Ok();
+        }
+    }
+}
---
+++ WebApi/RewriteMe.WebApi/Extensions/RegisterUserModelExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RewriteMe.Domain.UserManagement;
+using RewriteMe.WebApi.Models;
+
+namespace RewriteMe.WebApi.Extensions
+{
+    public static class RegisterUserModelExtensions
+    {
+        public static User ToUser(this RegisterUserModel registerUserModel)
+        {
+            return new User
+            {
+                Id = Guid.NewGuid(),
+                Username = registerUserModel.Username,
+                FirstName = registerUserModel.FirstName,
+                LastName = registerUserModel.LastName
+            };
+        }
+    }
+}
