a8b384253e3ad38bd9558ec8e97eab2813c8245c
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-03-01 22:28:28 +0100

    Create User and Authentication controller.

--- WebApi/RewriteMe.WebApi/Controllers/AuthenticationController.cs
+++ WebApi/RewriteMe.WebApi/Controllers/AuthenticationController.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using RewriteMe.Domain.Interfaces.Services;
+using RewriteMe.Domain.Settings;
+using RewriteMe.WebApi.Models;
 
 namespace RewriteMe.WebApi.Controllers
 {
@@ -7,5 +16,46 @@ namespace RewriteMe.WebApi.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        private readonly IAuthenticationService _authenticationService;
+        private readonly AppSettings _appSettings;
+
+        public AuthenticationController(
+            IAuthenticationService authenticationService,
+            IOptions<AppSettings> options)
+        {
+            _authenticationService = authenticationService;
+            _appSettings = options.Value;
+        }
+
+        [HttpPost("/api/authenticate")]
+        public IActionResult Authenticate([FromBody] AuthenticationModel authenticationModel)
+        {
+            var user = _authenticationService.Authenticate(authenticationModel.Username, authenticationModel.Password);
+            if (user == null)
+                return BadRequest();
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new
+            {
+                Id = Guid.NewGuid(),
+                Username = user.Username,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = tokenString
+            });
+        }
     }
 }
