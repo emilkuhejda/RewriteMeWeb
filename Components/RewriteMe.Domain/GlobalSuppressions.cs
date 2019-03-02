f7a5de5d77921805a80036fcb21fb8db76389d1b
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-03-02 11:00:18 +0100

    Add stylecop analyzers.

---
+++ Components/RewriteMe.Domain/GlobalSuppressions.cs
@@ -0,0 +1,22 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "Array is OK", Scope = "member", Target = "~P:RewriteMe.Domain.UserManagement.User.PasswordHash")]
+[assembly: SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "Array is OK", Scope = "member", Target = "~P:RewriteMe.Domain.UserManagement.User.PasswordSalt")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Project_id")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Private_key_id")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Private_key")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Client_email")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Client_id")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Auth_uri")]
+[assembly: SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Auth_uri")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Token_uri")]
+[assembly: SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Token_uri")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Auth_provider_x509_cert_url")]
+[assembly: SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Auth_provider_x509_cert_url")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Client_x509_cert_url")]
+[assembly: SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "By design", Scope = "member", Target = "~P:RewriteMe.Domain.Settings.SpeechCredentials.Client_x509_cert_url")]
