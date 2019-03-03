4db69e9c512857d68afeccc98fda6770e046b594
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-03-03 09:51:31 +0100

    Create transcribe item entity.

---
+++ Components/RewriteMe.DataAccess/EntitiesConfiguration/TranscribeItemEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RewriteMe.DataAccess.Entities;
+
+namespace RewriteMe.DataAccess.EntitiesConfiguration
+{
+    public class TranscribeItemEntityConfiguration : IEntityTypeConfiguration<TranscribeItemEntity>
+    {
+        public void Configure(EntityTypeBuilder<TranscribeItemEntity> builder)
+        {
+            builder.ToTable("TranscribeItem");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.FileItemId).IsRequired();
+            builder.Property(x => x.Transcript).IsRequired();
+            builder.Property(x => x.Source).IsRequired();
+        }
+    }
+}
