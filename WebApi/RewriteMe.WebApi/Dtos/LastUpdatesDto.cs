843233eadc61f13b59cf32ccf63a3577c798fd33
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-05-17 21:21:17 +0200

    Mark DTO entities with required attribute. (#6)

--- WebApi/RewriteMe.WebApi/Dtos/LastUpdatesDto.cs
+++ WebApi/RewriteMe.WebApi/Dtos/LastUpdatesDto.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RewriteMe.WebApi.Dtos
 {
     public class LastUpdatesDto
     {
+        [Required]
         public DateTime FileItem { get; set; }
 
+        [Required]
         public DateTime TranscribeItem { get; set; }
 
+        [Required]
         public DateTime UserSubscription { get; set; }
     }
 }
