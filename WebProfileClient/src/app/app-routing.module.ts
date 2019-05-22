21e0fb31506c0d5e01f7e9ca547e69cea85935ae
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-05-22 18:09:09 +0200

    Create login component.

--- WebProfileClient/src/app/app-routing.module.ts
+++ WebProfileClient/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent }
+
 ];
 
 @NgModule({
@@ -13,5 +16,6 @@ const routes: Routes = [
 export class AppRoutingModule { }
 
 export const routingComponents = [
-    HomeComponent
+    HomeComponent,
+    LoginComponent
 ]
