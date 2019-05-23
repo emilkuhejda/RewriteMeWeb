1cfdd71d91e41ae9602ade7cee3ead7ba3adcc8d
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-05-23 19:13:36 +0200

    Create login button in the main page.

--- WebClient/src/app/app.component.ts
+++ WebClient/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { MsalService } from './_services/msal.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WebClient';
+    title = 'WebClient';
+
+    constructor(private msalService: MsalService) { }
+
+    login(): void {
+        this.msalService.login();
+    }
+
+    logout(): void {
+        this.msalService.logout();
+    }
+
+    isUserLoggedIn(): boolean {
+        return this.msalService.isLoggedIn();
+    }
 }
