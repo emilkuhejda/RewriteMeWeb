08464f9620e4c50365f4682097943e64c5831b42
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-07-21 16:05:21 +0200

    Upload file to Web API.

--- WebProfileClient/src/app/_services/file-item.service.ts
+++ WebProfileClient/src/app/_services/file-item.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
 import { CommonVariables } from '../_config/common-variables';
 import { FileItem } from '../_models/file-item';
 import { map } from 'rxjs/operators';
@@ -10,14 +10,22 @@ import { FileItemMapper } from '../_mappers/file-item-mapper';
 })
 export class FileItemService {
     constructor(private http: HttpClient) { }
-    
+
     get(fileItemId: string) {
         return this.http.get<FileItem>(CommonVariables.ApiUrl + CommonVariables.ApiFileItemsPath + fileItemId).pipe(map(FileItemMapper.convert));
     }
-    
+
     getAll() {
         let params = new HttpParams();
         params = params.append('applicationId', CommonVariables.ApplicationId);
         return this.http.get<FileItem[]>(CommonVariables.ApiUrl + CommonVariables.ApiFileItemsPath, { params: params }).pipe(map(FileItemMapper.convertAll));
+    }
+
+    create(formData) {
+        let uploadRequest = new HttpRequest("POST", CommonVariables.ApiUrl + CommonVariables.ApiCreateFileItemPath, formData, {
+            reportProgress: true
+        });
+
+        return this.http.request(uploadRequest);
     }
 }
--- WebProfileClient/src/app/files/create-file/create-file.component.ts
+++ WebProfileClient/src/app/files/create-file/create-file.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FileItemService } from 'src/app/_services/file-item.service';
 import { AlertService } from 'src/app/_services/alert.service';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { ErrorResponse } from 'src/app/_models/error-response';
 
 @Component({
     selector: 'app-create-file',
@@ -45,6 +47,55 @@ export class CreateFileComponent implements OnInit {
     }
 
     onSubmit(files) {
-        console.log(files);
+        this.alertService.clear();
+        if (files.length === 0) {
+            this.alertService.error("File is required");
+            return;
+        }
+
+        if (this.controls.language.value === "") {
+            this.alertService.error("Language is required");
+            return;
+        }
+
+        this.submitted = true;
+        if (this.createFileForm.invalid)
+            return;
+
+        this.loading = true;
+
+        let formData = new FormData();
+        formData.append("name", this.controls.name.value);
+        formData.append("language", this.controls.language.value);
+
+        for (let file of files) {
+            formData.append(file.name, file)
+        }
+
+        this.fileItemService.create(formData)
+            .subscribe(
+                event => {
+                    if (event.type == HttpEventType.UploadProgress) {
+                        this.progress = Math.round(100 * event.loaded / event.total);
+                    } else if (event instanceof HttpResponse) {
+                        this.alertService.success("Project was successfully created", true);
+                        this.router.navigate(["/files"]);
+                    }
+                },
+                (err: ErrorResponse) => {
+                    let error = err.message;
+                    if (err.status === 400)
+                        error = "Uploaded file not found";
+
+                    if (err.status === 406)
+                        error = "Language is not supported";
+
+                    if (err.status === 415)
+                        error = "Audio file is not supported";
+
+                    this.alertService.error(error);
+                    this.loading = false;
+                }
+            );
     }
 }
