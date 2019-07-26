6ab9349c3ada535e275facce343a29ea33a05045
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-07-26 08:36:18 +0200

    Add control panel.
    Renaming.
    
    Renaming.

---
+++ WebApi/RewriteMe.WebApi/Controllers/ControlPanelController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RewriteMe.WebApi.Controllers
+{
+    public class ControlPanelController : Controller
+    {
+        [HttpGet("/control-panel")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/control-panel/privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
