a783588a5c49e77d692d1408696aea76b2f7fea7
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-07-28 15:56:44 +0200

    Create users view. (#13)
    * Create users view.
    
    * Create datatable.
    
    * Code refactoring.
    
    * Create subscription page.
    
    * Add html to dashboard.
    
    * Create dashboard.

--- WebControlPanelClient/src/app/home/users/users.component.ts
+++ WebControlPanelClient/src/app/home/users/users.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/_service/user.service';
+import { User } from 'src/app/_models/user';
+import { AlertService } from 'src/app/_services/alert.service';
+import { ErrorResponse } from 'src/app/_models/error-response';
+import * as $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4';
 
 @Component({
     selector: 'app-users',
@@ -7,9 +13,49 @@ import { UserService } from 'src/app/_service/user.service';
     styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-    constructor(private userService: UserService) { }
+    users: User[];
+    private tableWidget: any;
+
+    constructor(
+        private userService: UserService,
+        private alertService: AlertService) { }
 
     ngOnInit() {
-        this.userService.getAll().subscribe(data => { });
+        this.userService.getAll().subscribe(
+            users => {
+                this.users = users.sort(this.orderUsers);
+                this.reInitDatatable();
+            },
+            (err: ErrorResponse) => {
+                this.alertService.error(err.message);
+            });
+    }
+
+    ngAfterViewInit() {
+        this.initDatatable();
+    }
+
+    private initDatatable(): void {
+        let table: any = $('#dataTable');
+        this.tableWidget = table.DataTable();
+    }
+
+    private reInitDatatable(): void {
+        if (this.tableWidget) {
+            this.tableWidget.destroy();
+            this.tableWidget = null;
+        }
+
+        setTimeout(this.initDatatable, 0);
+    }
+
+    private orderUsers(a: User, b: User): number {
+        if (a.email > b.email)
+            return 1;
+
+        if (b.email > a.email)
+            return -1;
+
+        return 0;
     }
 }
