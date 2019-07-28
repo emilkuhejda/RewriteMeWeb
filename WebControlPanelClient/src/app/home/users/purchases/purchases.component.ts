e999050d14d861a118f7b57872e23ef8b7b5c542
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-07-28 17:27:06 +0200

    Billing purchases page.

--- WebControlPanelClient/src/app/home/users/purchases/purchases.component.ts
+++ WebControlPanelClient/src/app/home/users/purchases/purchases.component.ts
@@ -4,6 +4,9 @@ import { BillingPurchaseService } from 'src/app/_services/billing-purchase.servi
 import { AlertService } from 'src/app/_services/alert.service';
 import { ErrorResponse } from 'src/app/_models/error-response';
 import { BillingPurchase } from 'src/app/_models/billing-purchase';
+import * as $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4';
 
 @Component({
     selector: 'app-purchases',
@@ -11,6 +14,9 @@ import { BillingPurchase } from 'src/app/_models/billing-purchase';
     styleUrls: ['./purchases.component.css']
 })
 export class PurchasesComponent implements OnInit {
+    private tableWidget: any;
+    billingPurchases: BillingPurchase[];
+
     constructor(
         private route: ActivatedRoute,
         private billingPurchaseService: BillingPurchaseService,
@@ -20,11 +26,35 @@ export class PurchasesComponent implements OnInit {
         this.route.paramMap.subscribe(paramMap => {
             let userId = paramMap.get("userId");
             this.billingPurchaseService.getAll(userId).subscribe(
-                (billingPurchases: BillingPurchase[]) => { },
+                (billingPurchases: BillingPurchase[]) => {
+                    this.billingPurchases = billingPurchases.sort((a, b) => {
+                        return <any>new Date(b.transactionDateUtc) - <any>new Date(a.transactionDateUtc);
+                    });
+
+                    this.reInitDatatable();
+                },
                 (err: ErrorResponse) => {
                     this.alertService.error(err.message);
                 }
             );
         });
+    }
+
+    ngAfterViewInit() {
+        this.initDatatable();
+    }
+
+    private initDatatable(): void {
+        let table: any = $('#dataTable');
+        this.tableWidget = table.DataTable();
+    }
+
+    private reInitDatatable(): void {
+        if (this.tableWidget) {
+            this.tableWidget.destroy();
+            this.tableWidget = null;
+        }
+
+        setTimeout(this.initDatatable, 0);
     }
 }
