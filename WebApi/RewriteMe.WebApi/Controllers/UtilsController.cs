e05dfca0926e75a1f108b565ae5f746dceff1789
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-08-04 16:37:46 +0200

    Create IsAlive endpoint. (#19)

---
+++ WebApi/RewriteMe.WebApi/Controllers/UtilsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace RewriteMe.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [Authorize]
+    [ApiController]
+    public class UtilsController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("/api/is-alive")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [SwaggerOperation(OperationId = "IsAlive")]
+        public IActionResult IsAlive()
+        {
+            return Ok(true);
+        }
+    }
+}
