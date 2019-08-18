2a41c209f1aed6711b3f9686c15e936a1c5fc794
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-08-18 13:49:02 +0200

    Add terms page.

--- WebClient/src/app/app-routing.module.ts
+++ WebClient/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterUserComponent } from './register-user/register-user.component';
 import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'register-user', component: RegisterUserComponent },
     { path: 'privacy', component: PrivacyComponent },
+    { path: 'terms', component: TermsComponent },
     { path: '404', component: NotFoundComponent },
     { path: '**', redirectTo: '404' }
 ];
@@ -23,5 +25,6 @@ export const routingComponents = [
     HomeComponent,
     RegisterUserComponent,
     PrivacyComponent,
+    TermsComponent,
     NotFoundComponent
 ]
