77237be9cac56ef82c9a0fdfb91ba5da2b94ac07
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-09-09 18:08:20 +0200

    Restore files.

--- WebControlPanelClient/src/app/_services/file-item.service.ts
+++ WebControlPanelClient/src/app/_services/file-item.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { RoutingService } from './routing.service';
 import { Observable } from 'rxjs';
 import { FileItem } from '../_models/file-item';
@@ -16,5 +16,13 @@ export class FileItemService {
 
     getAll(userId: string): Observable<FileItem[]> {
         return this.http.get<FileItem[]>(this.routingService.getFileItemsUri() + userId).pipe(map(FileItemMapper.convertAll))
+    }
+
+    restore(userId: string, fileItemId: string) {
+        let params = new HttpParams();
+        params = params.append('userId', userId);
+        params = params.append('fileItemId', fileItemId);
+
+        return this.http.put(this.routingService.getRestoreFileItemUri(), null, { params: params });
     }
 }
--- WebControlPanelClient/src/app/home/users/files/files.component.ts
+++ WebControlPanelClient/src/app/home/users/files/files.component.ts
@@ -5,6 +5,8 @@ import { FileItemService } from 'src/app/_services/file-item.service';
 import { AlertService } from 'src/app/_services/alert.service';
 import { FileItem } from 'src/app/_models/file-item';
 import { ErrorResponse } from 'src/app/_models/error-response';
+import { GecoDialog } from 'angular-dynamic-dialog';
+import { DialogComponent } from 'src/app/_directives/dialog/dialog.component';
 
 @Component({
     selector: 'app-files',
@@ -12,6 +14,7 @@ import { ErrorResponse } from 'src/app/_models/error-response';
     styleUrls: ['./files.component.css']
 })
 export class FilesComponent implements OnInit {
+    private userId: string;
     fileItems: FileItem[] = [];
     deletedFileItems: FileItem[] = [];
     permanentlyDeletedFileItems: FileItem[] = [];
@@ -20,30 +23,60 @@ export class FilesComponent implements OnInit {
         private location: Location,
         private route: ActivatedRoute,
         private fileItemService: FileItemService,
-        private alertService: AlertService) { }
+        private alertService: AlertService,
+        private modal: GecoDialog) { }
 
     ngOnInit() {
         this.route.paramMap.subscribe(paramMap => {
-            let userId = paramMap.get("userId");
-            this.fileItemService.getAll(userId).subscribe(
-                (fileItems: FileItem[]) => {
-                    fileItems.sort((a, b) => {
-                        return <any>new Date(b.dateCreated) - <any>new Date(a.dateCreated);
-                    });
-
-                    this.fileItems = fileItems.filter(x => !x.isDeleted && !x.isPermanentlyDeleted);
-                    this.deletedFileItems = fileItems.filter(x => x.isDeleted && !x.isPermanentlyDeleted);
-                    this.permanentlyDeletedFileItems = fileItems.filter(x => x.isDeleted && x.isPermanentlyDeleted);
-
-                    console.log(fileItems);
-                },
-                (err: ErrorResponse) => {
-                    this.alertService.error(err.message);
-                });
+            this.userId = paramMap.get("userId");
+            this.initialize();
         });
     }
 
     goBack() {
         this.location.back();
+    }
+
+    restore(fileItem: FileItem) {
+        this.alertService.clear();
+        let onAccept = (dialogComponent: DialogComponent) => {
+            this.fileItemService.restore(this.userId, fileItem.id).subscribe(
+                () => {
+                    this.initialize();
+                },
+                (err: ErrorResponse) => {
+                    this.alertService.error(err.message);
+                })
+                .add(() => dialogComponent.close());
+        };
+
+        let data = {
+            title: `Restore ${fileItem.name}`,
+            message: `Do you really want to restore file '${fileItem.name}'?`,
+            onAccept: onAccept
+        };
+
+        let modal = this.modal.openDialog(DialogComponent, {
+            data: data,
+            useStyles: 'none'
+        });
+
+        modal.onClosedModal().subscribe();
+    }
+
+    private initialize(): void {
+        this.fileItemService.getAll(this.userId).subscribe(
+            (fileItems: FileItem[]) => {
+                fileItems.sort((a, b) => {
+                    return <any>new Date(b.dateCreated) - <any>new Date(a.dateCreated);
+                });
+
+                this.fileItems = fileItems.filter(x => !x.isDeleted && !x.isPermanentlyDeleted);
+                this.deletedFileItems = fileItems.filter(x => x.isDeleted && !x.isPermanentlyDeleted);
+                this.permanentlyDeletedFileItems = fileItems.filter(x => x.isDeleted && x.isPermanentlyDeleted);
+            },
+            (err: ErrorResponse) => {
+                this.alertService.error(err.message);
+            });
     }
 }
