74b9ddd982f17db09f13783351735370bf852399
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-10-02 07:25:14 +0200

    Create interval value entity.

---
+++ Components/RewriteMe.DataAccess/Repositories/InternalValueRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RewriteMe.DataAccess.Entities;
+using RewriteMe.Domain.Interfaces.Repositories;
+
+namespace RewriteMe.DataAccess.Repositories
+{
+    public class InternalValueRepository : IInternalValueRepository
+    {
+        private readonly IDbContextFactory _contextFactory;
+
+        public InternalValueRepository(IDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<string> GetValue(string key)
+        {
+            using (var context = _contextFactory.Create())
+            {
+                var entity = await context.InternalValues.FirstOrDefaultAsync(x => x.Key == key).ConfigureAwait(false);
+                return entity?.Value;
+            }
+        }
+
+        public async Task UpdateValue(string key, string value)
+        {
+            using (var context = _contextFactory.Create())
+            {
+                var entity = await context.InternalValues.FirstOrDefaultAsync(x => x.Key == key).ConfigureAwait(false);
+                if (entity == null)
+                {
+                    entity = new InternalValueEntity { Key = key, Value = value };
+                    await context.InternalValues.AddAsync(entity).ConfigureAwait(false);
+                }
+                else
+                {
+                    entity.Value = value;
+                }
+
+                await context.SaveChangesAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
