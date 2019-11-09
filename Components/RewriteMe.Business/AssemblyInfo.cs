d9feec1f72ac5024231cf1c74beed212052e4aba
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-11-09 19:25:41 +0100

    Rename and replace resource to Voicipher. (#69)
    * Upate app settings.
    
    * Update config files.
    
    * Update AppSettings file.
    
    * Update settings files.
    
    * Add assembly info files.

---
+++ Components/RewriteMe.Business/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("RewriteMe.Business")]
+[assembly: AssemblyProduct("RewriteMe.Business")]
+[assembly: CLSCompliant(false)]
---
+++ Components/RewriteMe.DataAccess/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("RewriteMe.DataAccess")]
+[assembly: AssemblyProduct("RewriteMe.DataAccess")]
+[assembly: CLSCompliant(false)]
