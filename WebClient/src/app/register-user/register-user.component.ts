b0b1fd4a518de73f0267d1a6a72802180030e7c9
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-11-16 10:06:44 +0100

    Update new template for Web client. (#72)
    * Replace template.
    
    * Update loading scripts.
    
    * Update Pages.
    
    * Update 404 page.
    
    * Update register page.

--- WebClient/src/app/register-user/register-user.component.ts
+++ WebClient/src/app/register-user/register-user.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MsalService } from '../_services/msal.service';
 import { UserService } from '../_services/user.service';
 import { CommonVariables } from '../_config/common-variables';
 import { Router } from '@angular/router';
 import { RoutingService } from '../_services/routing.service';
 import { UserRegistration } from '../_models/user-registration';
+import { DynamicScriptLoaderService } from '../_services/dynamic-script-loader.service';
 
 @Component({
     selector: 'app-register-user',
     templateUrl: './register-user.component.html',
     styleUrls: ['./register-user.component.css']
 })
-export class RegisterUserComponent implements OnInit {
+export class RegisterUserComponent implements OnInit, OnDestroy {
+    private scriptKey: string = "script";
+
     constructor(
         private router: Router,
         private routingService: RoutingService,
         private userService: UserService,
-        private msalService: MsalService) { }
+        private msalService: MsalService,
+        private dynamicScriptLoaderService: DynamicScriptLoaderService) { }
 
     ngOnInit() {
         let isLoggedIn = this.msalService.isLoggedIn();
@@ -25,6 +29,20 @@ export class RegisterUserComponent implements OnInit {
         } else {
             this.register();
         }
+
+        this.loadScripts();
+    }
+
+    ngOnDestroy(): void {
+        this.unloadScripts();
+    }
+
+    private loadScripts() {
+        this.dynamicScriptLoaderService.load(this.scriptKey);
+    }
+
+    private unloadScripts() {
+        this.dynamicScriptLoaderService.remove(this.scriptKey);
     }
 
     private register(): void {
