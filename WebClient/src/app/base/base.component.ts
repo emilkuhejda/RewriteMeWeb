6cd180e9443667b17477b05bc93aedd62c3a219c
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-11-24 16:44:24 +0100

    Create base component.

---
+++ WebClient/src/app/base/base.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DynamicScriptLoaderService } from '../_services/dynamic-script-loader.service';
+
+@Component({
+    selector: 'app-base',
+    template: '',
+    styleUrls: ['./base.component.css']
+})
+export class BaseComponent implements OnInit, OnDestroy {
+    private scriptKey: string = "script";
+
+    constructor(protected dynamicScriptLoaderService: DynamicScriptLoaderService) { }
+
+    ngOnInit() {
+        console.log("base on init");
+        this.loadScripts();
+    }
+
+    ngOnDestroy() {
+        this.unloadScripts();
+    }
+
+    protected loadScripts() {
+        this.dynamicScriptLoaderService.load(this.scriptKey);
+    }
+
+    protected unloadScripts() {
+        this.dynamicScriptLoaderService.remove(this.scriptKey);
+    }
+}
--- WebClient/src/app/contact/contact.component.ts
+++ WebClient/src/app/contact/contact.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DynamicScriptLoaderService } from '../_services/dynamic-script-loader.service';
+import { BaseComponent } from '../base/base.component';
 
 @Component({
 	selector: 'app-contact',
 	templateUrl: './contact.component.html',
 	styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
-	constructor() { }
-
-	ngOnInit() {
+export class ContactComponent extends BaseComponent {
+	constructor(protected dynamicScriptLoaderService: DynamicScriptLoaderService) {
+		super(dynamicScriptLoaderService);
 	}
 }
