5e25ccc12f1e9143ce393cb5de93771fe2072068
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-11-24 18:33:14 +0100

    Update contact form page.

--- WebClient/src/app/contact/contact.component.ts
+++ WebClient/src/app/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DynamicScriptLoaderService } from '../_services/dynamic-script-loader.service';
 import { BaseComponent } from '../base/base.component';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ContactFormService } from '../_services/contact-form.service';
 
 @Component({
 	selector: 'app-contact',
@@ -10,7 +12,58 @@ import { BaseComponent } from '../base/base.component';
 export class ContactComponent extends BaseComponent {
 	protected mapScriptKey: string = "map";
 
-	constructor(protected dynamicScriptLoaderService: DynamicScriptLoaderService) {
+	contactForm: FormGroup;
+	submitted: boolean;
+	loading: boolean;
+	successMessage: string;
+	errorMessage: string;
+
+	constructor(
+		private formBuilder: FormBuilder,
+		private contactFormService: ContactFormService,
+		protected dynamicScriptLoaderService: DynamicScriptLoaderService) {
 		super(dynamicScriptLoaderService);
+	}
+
+	ngOnInit() {
+		this.contactForm = this.formBuilder.group({
+			name: ['', Validators.required],
+			email: ['', [Validators.required, Validators.email]],
+			message: ['', Validators.required]
+		});
+
+		this.loadDefaultScript();
+	}
+
+	get controls() {
+		return this.contactForm.controls;
+	}
+
+	onSubmit() {
+		this.submitted = true;
+		this.successMessage = "";
+		this.errorMessage = "";
+
+		if (this.contactForm.invalid)
+			return;
+
+		this.loading = true;
+
+		let contactFormData = {
+			name: this.controls.name.value,
+			email: this.controls.email.value,
+			message: this.controls.message.value
+		};
+
+		this.contactFormService.create(contactFormData).subscribe(
+			() => {
+				this.successMessage = "Contact form was successfully sent.";
+			},
+			() => {
+				this.errorMessage = "Contact form was not successfully sent. Please, try it later."
+			}
+		).add(
+			() => { this.loading = false }
+		);
 	}
 }
--- WebClient/src/app/home/home.component.ts
+++ WebClient/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ContactFormService } from '../_services/contact-form.service';
 import { DynamicScriptLoaderService } from '../_services/dynamic-script-loader.service';
 import { BaseComponent } from '../base/base.component';
 
@@ -10,58 +8,7 @@ import { BaseComponent } from '../base/base.component';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent extends BaseComponent {
-    contactForm: FormGroup;
-    submitted: boolean;
-    loading: boolean;
-    successMessage: string;
-    errorMessage: string;
-
-    constructor(
-        private formBuilder: FormBuilder,
-        private contactFormService: ContactFormService,
-        protected dynamicScriptLoaderService: DynamicScriptLoaderService) {
+    constructor(protected dynamicScriptLoaderService: DynamicScriptLoaderService) {
         super(dynamicScriptLoaderService);
-    }
-
-    ngOnInit() {
-        this.contactForm = this.formBuilder.group({
-            name: ['', Validators.required],
-            email: ['', [Validators.required, Validators.email]],
-            message: ['', Validators.required]
-        });
-
-        this.loadDefaultScript();
-    }
-
-    get controls() {
-        return this.contactForm.controls;
-    }
-
-    onSubmit() {
-        this.submitted = true;
-        this.successMessage = "";
-        this.errorMessage = "";
-
-        if (this.contactForm.invalid)
-            return;
-
-        this.loading = true;
-
-        let contactFormData = {
-            name: this.controls.name.value,
-            email: this.controls.email.value,
-            message: this.controls.message.value
-        };
-
-        this.contactFormService.create(contactFormData).subscribe(
-            () => {
-                this.successMessage = "Contact form was successfully sent.";
-            },
-            () => {
-                this.errorMessage = "Contact form was not successfully sent. Please, try it later."
-            }
-        ).add(
-            () => { this.loading = false }
-        );
     }
 }
