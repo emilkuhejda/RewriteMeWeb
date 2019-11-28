a1ece34ee567ff3cf914a8f443084f89c1fd31b3
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-11-28 15:39:36 +0100

    Update to Web API .net core 3.0. (#79)
    * Update version .net core to 3.0.
    
    * Fix default filter provider.
    
    * Fix cors.
    
    * Autofact integration.
    
    * Code refactoring.
    
    * Code refactoring.
    
    * Fix swagger.
    
    * Remove filter.
    
    * Code refactoring.
    
    * Refactoring of autofac modules.
    
    * Move interfaces.
    
    * Update Web API.

---
+++ Components/RewriteMe.Business/BusinessModule.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using RewriteMe.Business.Managers;
+using RewriteMe.Business.Services;
+using RewriteMe.Domain.Interfaces.Managers;
+using RewriteMe.Domain.Interfaces.Services;
+
+namespace RewriteMe.Business
+{
+    public class BusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            RegisterServices(builder);
+        }
+
+        public static void RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterType<AdministratorService>().As<IAdministratorService>().InstancePerLifetimeScope();
+            builder.RegisterType<UserService>().As<IUserService>().InstancePerLifetimeScope();
+            builder.RegisterType<FileItemService>().As<IFileItemService>().InstancePerLifetimeScope();
+            builder.RegisterType<FileItemSourceService>().As<IFileItemSourceService>().InstancePerLifetimeScope();
+            builder.RegisterType<TranscribeItemService>().As<ITranscribeItemService>().InstancePerLifetimeScope();
+            builder.RegisterType<TranscribeItemSourceService>().As<ITranscribeItemSourceService>().InstancePerLifetimeScope();
+            builder.RegisterType<RecognizedAudioSampleService>().As<IRecognizedAudioSampleService>().InstancePerLifetimeScope();
+            builder.RegisterType<SpeechResultService>().As<ISpeechResultService>().InstancePerLifetimeScope();
+            builder.RegisterType<UserSubscriptionService>().As<IUserSubscriptionService>().InstancePerLifetimeScope();
+            builder.RegisterType<ApplicationLogService>().As<IApplicationLogService>().InstancePerLifetimeScope();
+            builder.RegisterType<ContactFormService>().As<IContactFormService>().InstancePerLifetimeScope();
+            builder.RegisterType<InformationMessageService>().As<IInformationMessageService>().InstancePerLifetimeScope();
+            builder.RegisterType<LanguageVersionService>().As<ILanguageVersionService>().InstancePerLifetimeScope();
+            builder.RegisterType<UserDeviceService>().As<IUserDeviceService>().InstancePerLifetimeScope();
+            builder.RegisterType<InternalValueService>().As<IInternalValueService>().InstancePerLifetimeScope();
+            builder.RegisterType<DatabaseService>().As<IDatabaseService>().InstancePerLifetimeScope();
+
+            builder.RegisterType<SpeechRecognitionService>().As<ISpeechRecognitionService>().InstancePerLifetimeScope();
+            builder.RegisterType<WavFileService>().As<IWavFileService>().InstancePerLifetimeScope();
+            builder.RegisterType<BillingPurchaseService>().As<IBillingPurchaseService>().InstancePerLifetimeScope();
+            builder.RegisterType<AuthenticationService>().As<IAuthenticationService>().InstancePerLifetimeScope();
+            builder.RegisterType<PushNotificationsService>().As<IPushNotificationsService>().InstancePerLifetimeScope();
+            builder.RegisterType<CleanUpService>().As<ICleanUpService>().InstancePerLifetimeScope();
+
+            builder.RegisterType<SpeechRecognitionManager>().As<ISpeechRecognitionManager>().InstancePerLifetimeScope();
+            builder.RegisterType<WavFileManager>().As<IWavFileManager>().InstancePerLifetimeScope();
+        }
+    }
+}
