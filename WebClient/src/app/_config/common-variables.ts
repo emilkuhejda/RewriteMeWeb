bb45c062fda33c6f41987838a695fc604fb0a033
emilkuhejda <emil.kuhejda@gmail.com>
2019-11-29 15:12:09 +0100

    Update production endpoint.

--- WebClient/src/app/_config/common-variables.ts
+++ WebClient/src/app/_config/common-variables.ts
@@ -5,6 +5,6 @@ export class CommonVariables {
     public static B2CAccessTokenKey: string = "b2c.access.token";
     public static B2CAccessErrorKey: string = "b2c.access.error";
     public static ApiUriDevelopment: string = "https://localhost:44357/";
-    public static ApiUriProduction: string = "https://rewrite-me.com/";
-    public static LoginRedirectUri: string = "https://rewrite-me.com/home/register-user/";
+    public static ApiUriProduction: string = "https://voicipher.com/";
+    public static LoginRedirectUri: string = "https://voicipher.com/home/register-user/";
 }
