9cfac9ef076b330b67b7625d2335b784d51730fe
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-11-29 14:08:13 +0100

    Add new product subscription. (#81)

--- Components/RewriteMe.Domain/Transcription/SubscriptionProducts.cs
+++ Components/RewriteMe.Domain/Transcription/SubscriptionProducts.cs
@@ -4,10 +4,12 @@ namespace RewriteMe.Domain.Transcription
 {
     public static class SubscriptionProducts
     {
-        public static SubscriptionProduct ProductBasic { get; } = new SubscriptionProduct("product.subscription.basic", TimeSpan.FromHours(1));
+        public static SubscriptionProduct ProductBasic { get; } = new SubscriptionProduct("product.subscription.v1.basic", TimeSpan.FromHours(1));
 
-        public static SubscriptionProduct ProductAdvanced { get; } = new SubscriptionProduct("product.subscription.advanced", TimeSpan.FromHours(10));
+        public static SubscriptionProduct ProductStandard { get; } = new SubscriptionProduct("product.subscription.v1.standard", TimeSpan.FromHours(5));
 
-        public static SubscriptionProduct[] All { get; } = { ProductBasic, ProductAdvanced };
+        public static SubscriptionProduct ProductAdvanced { get; } = new SubscriptionProduct("product.subscription.v1.advanced", TimeSpan.FromHours(10));
+
+        public static SubscriptionProduct[] All { get; } = { ProductBasic, ProductStandard, ProductAdvanced };
     }
 }
