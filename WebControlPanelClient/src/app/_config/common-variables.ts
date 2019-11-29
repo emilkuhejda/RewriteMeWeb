66dcb6b652dfd5b74e580fd1dc6f209dec28e5f7
Emil Kuhejda <emil.kuhejda@gmail.com>
2019-11-29 15:12:34 +0100

    Update production endpoint. (#82)

--- WebControlPanelClient/src/app/_config/common-variables.ts
+++ WebControlPanelClient/src/app/_config/common-variables.ts
@@ -2,5 +2,5 @@ export class CommonVariables {
     public static ApplicationId = "8affa70e-bcbf-4a94-a5e1-a2970dc9014a";
     public static CurrentIdentity: string = "current.identity";
     public static ApiUriDevelopment: string = "https://localhost:44357/";
-    public static ApiUriProduction: string = "https://rewrite-me.com/";
+    public static ApiUriProduction: string = "https://voicipher.com/";
 }
