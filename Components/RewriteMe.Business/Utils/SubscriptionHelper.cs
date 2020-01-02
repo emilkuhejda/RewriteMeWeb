7127ac125ca5dbe0655f2c7c75624c405c52fd08
emilkuhejda <emil.kuhejda@gmail.com>
2020-01-02 21:55:40 +0100

    Update free subscription to 10 minutes.

---
+++ Components/RewriteMe.Business/Utils/SubscriptionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RewriteMe.Domain.Enums;
+using RewriteMe.Domain.Settings;
+
+namespace RewriteMe.Business.Utils
+{
+    public static class SubscriptionHelper
+    {
+        public static UserSubscription CreateFreeSubscription(Guid userId, Guid applicationId)
+        {
+            return new UserSubscription
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId,
+                ApplicationId = applicationId,
+                Time = TimeSpan.FromMinutes(10),
+                Operation = SubscriptionOperation.Add,
+                DateCreatedUtc = DateTime.UtcNow
+            };
+        }
+    }
+}
