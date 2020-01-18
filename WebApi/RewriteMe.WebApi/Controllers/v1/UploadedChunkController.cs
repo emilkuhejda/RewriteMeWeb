99ea8faadf40d828d842d5c7954a5fda8ebbe901
emilkuhejda <emil.kuhejda@gmail.com>
2020-01-18 10:38:44 +0100

    Code refactoring.

--- WebApi/RewriteMe.WebApi/Controllers/v1/UploadedChunkController.cs
+++ WebApi/RewriteMe.WebApi/Controllers/v1/UploadedChunkController.cs
@@ -136,7 +136,7 @@ namespace RewriteMe.WebApi.Controllers.V1
 
                 fileItem.ApplicationId = applicationId;
                 fileItem.OriginalSourceFileName = uploadedFile.FileName;
-                //fileItem.OriginalContentType = contentType;
+                fileItem.OriginalContentType = "contentType";
                 fileItem.Storage = storageSetting;
                 fileItem.TotalTime = totalTime.Value;
                 fileItem.DateUpdatedUtc = dateUpdated;
