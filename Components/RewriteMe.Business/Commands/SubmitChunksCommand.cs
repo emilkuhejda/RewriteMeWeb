ebf925260b97b4b22fe8c3957a41b8e9d30b2474
Emil Kuhejda <emil.kuhejda@gmail.com>
2020-02-19 09:24:45 +0100

    Code refactoring.

---
+++ Components/RewriteMe.Business/Commands/SubmitChunksCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RewriteMe.Domain.Dtos;
+using RewriteMe.Domain.Enums;
+
+namespace RewriteMe.Business.Commands
+{
+    public class SubmitChunksCommand : CommandBase<FileItemDto>
+    {
+        public Guid FileItemId { get; set; }
+
+        public int ChunksCount { get; set; }
+
+        public StorageSetting ChunksStorageSetting { get; set; }
+
+        public Guid ApplicationId { get; set; }
+    }
+}
---
+++ Components/RewriteMe.Business/Commands/UploadChunkFileCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using RewriteMe.Domain.Dtos;
+using RewriteMe.Domain.Enums;
+
+namespace RewriteMe.Business.Commands
+{
+    public class UploadChunkFileCommand : CommandBase<OkDto>
+    {
+        public Guid FileItemId { get; set; }
+
+        public int Order { get; set; }
+
+        public StorageSetting StorageSetting { get; set; }
+
+        public Guid ApplicationId { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
