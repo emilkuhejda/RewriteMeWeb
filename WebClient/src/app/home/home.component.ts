c4873960b368886e1a743edca567ec56d27f7fe2
emilkuhejda <emil.kuhejda@gmail.com>
2020-02-23 11:44:01 +0100

    Fix button on the home page.

--- WebClient/src/app/home/home.component.ts
+++ WebClient/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DynamicScriptLoaderService } from '../_services/dynamic-script-loader.service';
 import { BaseComponent } from '../base/base.component';
+import { MsalService } from '../_services/msal.service';
 
 @Component({
     selector: 'app-home',
@@ -8,7 +9,13 @@ import { BaseComponent } from '../base/base.component';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent extends BaseComponent {
-    constructor(protected dynamicScriptLoaderService: DynamicScriptLoaderService) {
+    constructor(
+        private msalService: MsalService,
+        protected dynamicScriptLoaderService: DynamicScriptLoaderService) {
         super(dynamicScriptLoaderService);
+    }
+
+    login(): void {
+        this.msalService.login();
     }
 }
