d8e87bbc9a2c5785033cc4cd5ae85ca8f21ba57a
emilkuhejda <emil.kuhejda@gmail.com>
2020-02-29 17:52:02 +0100

    Create endpoints.

---
+++ WebApi/RewriteMe.WebApi/Controllers/ControlPanel/V1/DeletedAccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RewriteMe.Domain.Enums;
+using RewriteMe.Domain.Interfaces.Services;
+
+namespace RewriteMe.WebApi.Controllers.ControlPanel.V1
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/control-panel/deleted-accounts")]
+    [Produces("application/json")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Authorize(Roles = nameof(Role.Admin))]
+    [ApiController]
+    public class DeletedAccountController : ControllerBase
+    {
+        private readonly IDeletedAccountService _deletedAccountService;
+
+        public DeletedAccountController(IDeletedAccountService deletedAccountService)
+        {
+            _deletedAccountService = deletedAccountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var deletedAccounts = await _deletedAccountService.GetAllAsync().ConfigureAwait(false);
+
+            return Ok(deletedAccounts);
+        }
+
+        [HttpDelete("{deletedAccountId}")]
+        public async Task<IActionResult> Delete(Guid deletedAccountId)
+        {
+            await _deletedAccountService.DeleteAsync(deletedAccountId).ConfigureAwait(false);
+
+            return Ok();
+        }
+    }
+}
