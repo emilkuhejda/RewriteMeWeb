02b0f2fa2cc893c1de2f848f75de9443e39422b0
emilkuhejda <emil.kuhejda@gmail.com>
2020-02-29 18:05:57 +0100

    Create service.

--- WebControlPanelClient/src/app/home/deleted-accounts/deleted-accounts.component.ts
+++ WebControlPanelClient/src/app/home/deleted-accounts/deleted-accounts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeletedAccountService } from 'src/app/_services/deleted-account.service';
 
 @Component({
     selector: 'app-deleted-accounts',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./deleted-accounts.component.css']
 })
 export class DeletedAccountsComponent implements OnInit {
-    constructor() { }
+    constructor(private deletedAccountService: DeletedAccountService) { }
 
     ngOnInit() {
+        this.deletedAccountService.getAll().subscribe(accounts => {
+            console.log(accounts);
+        });
+
+        // this.deletedAccountService.delete("f9f01e95-ba8e-4cd4-93b1-347d5f213d54").subscribe();
     }
 }
