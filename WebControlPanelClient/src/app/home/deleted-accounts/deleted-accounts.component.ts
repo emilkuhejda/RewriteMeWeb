9344e35d684ea4286123433b5a857349a8f217e2
emilkuhejda <emil.kuhejda@gmail.com>
2020-03-01 10:39:47 +0100

    Deleted accounts page.

--- WebControlPanelClient/src/app/home/deleted-accounts/deleted-accounts.component.ts
+++ WebControlPanelClient/src/app/home/deleted-accounts/deleted-accounts.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DeletedAccountService } from 'src/app/_services/deleted-account.service';
+import { DeletedAccount } from 'src/app/_models/deleted-account';
+import { GecoDialog } from 'angular-dynamic-dialog';
+import { AlertService } from 'src/app/_services/alert.service';
+import { DialogComponent } from 'src/app/_directives/dialog/dialog.component';
+import { ErrorResponse } from 'src/app/_models/error-response';
 
 @Component({
     selector: 'app-deleted-accounts',
@@ -7,13 +12,47 @@ import { DeletedAccountService } from 'src/app/_services/deleted-account.service
     styleUrls: ['./deleted-accounts.component.css']
 })
 export class DeletedAccountsComponent implements OnInit {
-    constructor(private deletedAccountService: DeletedAccountService) { }
+    deletedAccounts: DeletedAccount[];
+
+    constructor(
+        private deletedAccountService: DeletedAccountService,
+        private alertService: AlertService,
+        private modal: GecoDialog) { }
 
     ngOnInit() {
+        this.initialize();
+    }
+
+    initialize(): void {
         this.deletedAccountService.getAll().subscribe(accounts => {
-            console.log(accounts);
+            this.deletedAccounts = accounts;
+        });
+    }
+
+    delete(deletedAccount: DeletedAccount) {
+        this.alertService.clear();
+        let onAccept = (dialogComponent: DialogComponent) => {
+            this.deletedAccountService.delete(deletedAccount.id).subscribe(
+                () => {
+                    this.initialize();
+                },
+                (err: ErrorResponse) => {
+                    this.alertService.error(err.message);
+                })
+                .add(() => dialogComponent.close());
+        };
+
+        let data = {
+            title: `Delete user`,
+            message: `Do you really want to delete user '${deletedAccount.userId}'?`,
+            onAccept: onAccept
+        };
+
+        let modal = this.modal.openDialog(DialogComponent, {
+            data: data,
+            useStyles: 'none'
         });
 
-        this.deletedAccountService.delete("f9f01e95-ba8e-4cd4-93b1-347d5f213d54").subscribe();
+        modal.onClosedModal().subscribe();
     }
 }
