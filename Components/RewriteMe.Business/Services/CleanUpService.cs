010d65e5d4eba3a06c0a2e3cbf517a366d37ab2e
emilkuhejda <emil.kuhejda@gmail.com>
2020-03-15 12:13:33 +0100

    Code refactoring.

--- Components/RewriteMe.Business/Services/CleanUpService.cs
+++ Components/RewriteMe.Business/Services/CleanUpService.cs
@@ -7,23 +7,24 @@ using RewriteMe.Common.Utils;
 using RewriteMe.Domain.Enums;
 using RewriteMe.Domain.Interfaces.Repositories;
 using RewriteMe.Domain.Interfaces.Services;
+using Serilog;
 
 namespace RewriteMe.Business.Services
 {
     public class CleanUpService : ICleanUpService
     {
         private readonly IFileAccessService _fileAccessService;
-        private readonly IApplicationLogService _applicationLogService;
         private readonly IFileItemRepository _fileItemRepository;
+        private readonly ILogger _logger;
 
         public CleanUpService(
             IFileAccessService fileAccessService,
-            IApplicationLogService applicationLogService,
-            IFileItemRepository fileItemRepository)
+            IFileItemRepository fileItemRepository,
+            ILogger logger)
         {
             _fileAccessService = fileAccessService;
-            _applicationLogService = applicationLogService;
             _fileItemRepository = fileItemRepository;
+            _logger = logger;
         }
 
         public void CleanUp(DateTime deleteBefore, CleanUpSettings cleanUpSettings, bool forceCleanUp)
@@ -38,16 +39,17 @@ namespace RewriteMe.Business.Services
 
             try
             {
-                var message = $"Start cleaning file item sources at '{DateTime.UtcNow}' with settings: border datetime='{deleteBefore}', settings='{cleanUpSettings.ToString()}', force cleanup = '{forceCleanUp}'.";
-                await _applicationLogService.InfoAsync(message).ConfigureAwait(false);
+                _logger.Information($"Start cleaning file item sources at '{DateTime.UtcNow}' with settings: border datetime='{deleteBefore}', settings='{cleanUpSettings.ToString()}', force cleanup = '{forceCleanUp}'.");
 
                 var count = await CleanUpInternalAsync(deleteBefore, cleanUpSettings, forceCleanUp).ConfigureAwait(false);
 
-                await _applicationLogService.InfoAsync($"Clean up was successfully finished. {count} file items was processed.").ConfigureAwait(false);
+                _logger.Information($"Clean up was successfully finished. {count} file items was processed.");
             }
             catch (Exception ex)
             {
-                await _applicationLogService.ErrorAsync($"During cleanup error occurred.{Environment.NewLine}{ExceptionFormatter.FormatException(ex)}").ConfigureAwait(false);
+                _logger.Fatal($"During cleanup error occurred.");
+                _logger.Fatal(ExceptionFormatter.FormatException(ex));
+
                 throw;
             }
         }
