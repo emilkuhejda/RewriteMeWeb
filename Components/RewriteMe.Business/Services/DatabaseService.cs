72205adbe5cdb891022bc995e17698a941d2d15e
emilkuhejda <emil.kuhejda@gmail.com>
2020-04-03 16:25:12 +0200

    Log events in database service.

--- Components/RewriteMe.Business/Services/DatabaseService.cs
+++ Components/RewriteMe.Business/Services/DatabaseService.cs
@@ -1,26 +1,35 @@
 ﻿using System.Threading.Tasks;
 using RewriteMe.Domain.Interfaces.Repositories;
 using RewriteMe.Domain.Interfaces.Services;
+using Serilog;
 
 namespace RewriteMe.Business.Services
 {
     public class DatabaseService : IDatabaseService
     {
         private readonly IDatabaseRepository _databaseRepository;
+        private readonly ILogger _logger;
 
-        public DatabaseService(IDatabaseRepository databaseRepository)
+        public DatabaseService(
+            IDatabaseRepository databaseRepository,
+            ILogger logger)
         {
             _databaseRepository = databaseRepository;
+            _logger = logger.ForContext<DatabaseService>();
         }
 
         public async Task ResetAsync()
         {
             await _databaseRepository.ResetAsync().ConfigureAwait(false);
+
+            _logger.Information("Database was reset.");
         }
 
         public async Task DeleteDatabaseAsync()
         {
             await _databaseRepository.DeleteDatabaseAsync().ConfigureAwait(false);
+
+            _logger.Information("Database was deleted.");
         }
     }
 }
