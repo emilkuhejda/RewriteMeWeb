e4c3f4ab4e8b7ade9fdbae715af2326cf04e80cb
Emil Kuhejda <emil.kuhejda@gmail.com>
2020-04-25 12:04:54 +0200

    Fix deleting file items. (#138)
    * Fix deleting file items.
    
    * Fix deleting file items.

---
+++ Components/RewriteMe.DataAccess/Extensions/FileItemEntityExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RewriteMe.DataAccess.Entities;
+
+namespace RewriteMe.DataAccess.Extensions
+{
+    public static class FileItemEntityExtensions
+    {
+        public static FileItemEntity CreateDeletedEntity(this FileItemEntity fileItemEntity, Guid applicationId)
+        {
+            return new FileItemEntity
+            {
+                Id = fileItemEntity.Id,
+                UserId = fileItemEntity.UserId,
+                ApplicationId = applicationId,
+                Name = "--DELETED--",
+                FileName = "--DELETED--",
+                Language = string.Empty,
+                DateCreated = fileItemEntity.DateCreated,
+                DateUpdatedUtc = DateTime.UtcNow,
+                IsDeleted = true,
+                IsPermanentlyDeleted = true,
+                WasCleaned = true
+            };
+        }
+    }
+}
