7c695ccbb1b5a661a3178a00ccee32115b3ddc1b
Emil Kuhejda <emil.kuhejda@gmail.com>
2020-05-22 20:54:49 +0200

    Add audio channel to recognition file. (#147)

--- Components/RewriteMe.Domain/Transcription/WavPartialFile.cs
+++ Components/RewriteMe.Domain/Transcription/WavPartialFile.cs
@@ -8,6 +8,8 @@ namespace RewriteMe.Domain.Transcription
 
         public string Path { get; set; }
 
+        public int AudioChannels { get; set; }
+
         public TimeSpan StartTime { get; set; }
 
         public TimeSpan EndTime { get; set; }
