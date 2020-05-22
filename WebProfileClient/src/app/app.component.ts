3ffb26dfa5445eb5a70acd565eb25fc1ab83cc31
emilkuhejda <emil.kuhejda@gmail.com>
2020-05-22 21:58:23 +0200

    Fix handling messages.

--- WebProfileClient/src/app/app.component.ts
+++ WebProfileClient/src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MsalService } from './_services/msal.service';
+import { MessageCenterService } from './_services/message-center.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-    constructor(private msalService: MsalService) { }
+export class AppComponent implements OnInit, OnDestroy {
+    constructor(
+        private msalService: MsalService,
+        private messageCenterService: MessageCenterService) { }
 
     ngOnInit(): void {
+        this.messageCenterService.startConnection();
+    }
+
+    ngOnDestroy(): void {
+        this.messageCenterService.stopConnection();
     }
 
     logout(): void {
