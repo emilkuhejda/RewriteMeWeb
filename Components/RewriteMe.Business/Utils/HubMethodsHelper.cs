85b67e64faf6f7278a279f6a93aad8c13b090a26
emilkuhejda <emil.kuhejda@gmail.com>
2020-05-22 22:21:38 +0200

    Handle transcription error.

--- Components/RewriteMe.Business/Utils/HubMethodsHelper.cs
+++ Components/RewriteMe.Business/Utils/HubMethodsHelper.cs
@@ -6,6 +6,7 @@ namespace RewriteMe.Business.Utils
     {
         private const string RecognitionProgressChangedMethod = "recognition-progress";
         private const string RecognitionStateChangedMethod = "recognition-state";
+        private const string RecognitionErrorMethod = "recognition-error";
         private const string FilesListChangedMethod = "file-list";
 
         public static string GetRecognitionProgressChangedMethod(Guid userId)
@@ -16,6 +17,11 @@ namespace RewriteMe.Business.Utils
         public static string GetRecognitionStateChangedMethod(Guid userId)
         {
             return $"{RecognitionStateChangedMethod}-{userId}";
+        }
+
+        public static string GetRecognitionErrorMethod(Guid userId)
+        {
+            return $"{RecognitionErrorMethod}-{userId}";
         }
 
         public static string GetFilesListChangedMethod(Guid userId)
--- WebProfileClient/src/app/app.component.ts
+++ WebProfileClient/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MsalService } from './_services/msal.service';
 import { MessageCenterService } from './_services/message-center.service';
+import { AlertService } from './_services/alert.service';
 
 @Component({
     selector: 'app-root',
@@ -8,12 +9,20 @@ import { MessageCenterService } from './_services/message-center.service';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
+    private recognitionErrorMethod: string = "recognition-error";
+
     constructor(
         private msalService: MsalService,
-        private messageCenterService: MessageCenterService) { }
+        private messageCenterService: MessageCenterService,
+        private alertService: AlertService) { }
 
     ngOnInit(): void {
         this.messageCenterService.startConnection();
+        this.messageCenterService.addListener(this.recognitionErrorMethod, (fileName: string) => this.handleRecognitionErrorMethod(fileName, this.alertService));
+    }
+
+    private handleRecognitionErrorMethod(fileName: string, alertService) {
+        alertService.error(`File '${fileName}' was not successfully transcripted. Please try it again or contact us.`);
     }
 
     ngOnDestroy(): void {
