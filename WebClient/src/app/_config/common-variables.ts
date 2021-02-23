4cc5f78370fa8c934f6e0813639024516ce0d9b6
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-23 15:42:58 +0100

    Cache audio file source on client side.

--- WebClient/src/app/_config/common-variables.ts
+++ WebClient/src/app/_config/common-variables.ts
@@ -4,7 +4,7 @@ export class CommonVariables {
     public static AccessTokenKey: string = "access.token";
     public static B2CAccessTokenKey: string = "b2c.access.token";
     public static B2CAccessErrorKey: string = "b2c.access.error";
-    public static ApiUriDevelopment: string = "https://localhost:44357/";
+    public static ApiUriDevelopment: string = "https://localhost:5001/";
     public static ApiUriProduction: string = "https://voicipher.com/";
     public static LoginRedirectUri: string = "https://voicipher.com/home/register-user/";
 }
