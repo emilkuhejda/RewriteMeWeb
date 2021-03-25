51cdee37b7c4948bb90ba97fc3b94f316d2f4124
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-03-25 16:45:46 +0100

    Send audio type setting to transcribe endpoint. (#165)

--- WebProfileClient/src/app/_models/transcribe-model.ts
+++ WebProfileClient/src/app/_models/transcribe-model.ts
@@ -1,6 +1,7 @@
 export interface TranscribeModel {
     fileItemId: string;
     language: string;
+    isPhoneCall: boolean;
     isTimeFrame: boolean;
     startTime: number;
     endTime: number;
